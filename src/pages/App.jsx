6723aac209f794ed63f3960481bdca948afce397
Artiom-ai <11artemkubarev@gmail.com>
2025-07-28 21:32:58 +0300

    Update App.jsx
    Paths

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import Categories from "./Categories.jsx";
 import Learn from "./Learn.jsx";
+// если Review будет нужен:
+import Review from "./Review.jsx";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/learn/:category" element={<Learn />} />
+        <Route path="/review" element={<Review />} /> {/* если хочешь */}
       </Routes>
     </BrowserRouter>
   );
