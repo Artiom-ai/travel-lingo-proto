2da276aba3f39e248ad42c1838e9147e30bccb01
Artiom-ai <11artemkubarev@gmail.com>
2025-07-28 20:07:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Categories from "./pages/Categories.jsx";
+import Learn from "./pages/Learn.jsx";
+import Review from "./pages/Review.jsx";
+
 function App() {
   return (
-    <div>
-      <h1>Travel Lingo Proto</h1>
-      <p>Приложение запущено</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/learn/:category" element={<Learn />} />
+        <Route path="/review" element={<Review />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-console.log("trigger deploy");
