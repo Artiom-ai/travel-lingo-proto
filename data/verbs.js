e63d3b534926dad6584800c278cd173a90609ff7
Artiom-ai <11artemkubarev@gmail.com>
2025-07-28 20:10:15 +0300

    Create verbs.js

---
+++ data/verbs.js
@@ -0,0 +1,27 @@
+export const verbs = [
+  {
+    ru: "быть",
+    sw: "kuwa",
+    example: "Я хочу быть счастливым — Nataka kuwa na furaha",
+  },
+  {
+    ru: "иметь",
+    sw: "kuwa na",
+    example: "У меня есть друг — Nina rafiki",
+  },
+  {
+    ru: "делать",
+    sw: "kufanya",
+    example: "Что ты делаешь? — Unafanya nini?",
+  },
+  {
+    ru: "идти",
+    sw: "kwenda",
+    example: "Я иду домой — Ninaenda nyumbani",
+  },
+  {
+    ru: "есть (приём пищи)",
+    sw: "kula",
+    example: "Я ем рис — Nakula wali",
+  },
+];
