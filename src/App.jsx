3ced4a885029db7ae2fdce616db87b8ac05f0e1c
Artiom-ai <11artemkubarev@gmail.com>
2025-07-28 20:16:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Categories from "./pages/Categories.jsx";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Categories from "./pages/Categories.jsx";
 import Learn from "./pages/Learn.jsx";
-import Review from "./pages/Review.jsx";
 
 function App() {
   return (
@@ -11,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/learn/:category" element={<Learn />} />
-        <Route path="/review" element={<Review />} />
       </Routes>
     </BrowserRouter>
   );
